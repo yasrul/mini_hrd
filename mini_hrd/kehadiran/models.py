32392daa6dc6d729c22caf9d5e0396a6fccf48f4
Yasrul <yasrul@gmail.com>
2018-07-31 16:55:06 +0800

    20180731

--- mini_hrd/kehadiran/models.py
+++ mini_hrd/kehadiran/models.py
@@ -18,3 +18,19 @@ class Kehadiran (models.Model):
 	
 	def __str__(self):
 		return self.karyawan.nama
+		
+class Izin(models.Model):
+	JENIS_KEHADIRAN_CHOICES = (
+		('izin','Izin'),
+		('cuti','Cuti')
+	)
+	
+	karyawan = models.ForeignKey(Karyawan, on_delete=models.CASCADE)
+	jenis_kehadiran = models.CharField(max_length=20, choices=JENIS_KEHADIRAN_CHOICES)
+	waktu_mulai = models.DateField()
+	waktu_berhenti = models.DateField()
+	alasan = models.TextField()
+	disetujui = models.BooleanField(default=False)
+	
+	def __str__(self):
+		return self.karyawan.nama
--- mini_hrd/mini_hrd/urls.py
+++ mini_hrd/mini_hrd/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from homepage import views as homepage_views
+from karyawan import views as karyawan_views
+from kehadiran import views as kehadiran_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', karyawan_views.profil),
+    url(r'^login/', homepage_views.login_view),
+    url(r'^logout/', homepage_views.logout_view),
+    url(r'^daftar_hadir/', kehadiran_views.daftar_hadir),
+    url(r'^pengajuan_izin/', kehadiran_views.pengajuan_izin),
+    url(r'^daftar_izin/', kehadiran_views.daftar_izin)
 ]
