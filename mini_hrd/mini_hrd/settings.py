96e5f1ff7dac8cfe98bf1034e039e65f92e3239d
Yasrul <yasrul@gmail.com>
2018-08-08 16:08:42 +0800

    20180808

--- mini_hrd/mini_hrd/settings.py
+++ mini_hrd/mini_hrd/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mini_hrd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
+
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, "assets"),
+)
